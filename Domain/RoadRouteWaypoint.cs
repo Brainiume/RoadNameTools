afa40ee8cbed372e1b875e61a1a7a8567e9c4cb5
Brainiume <170275855+Brainiume@users.noreply.github.com>
2026-04-26 20:25:07 +1000

    JSON-writable saved route records and waypoints
    Add UI JSON serialization support for saved route data, contributed by bruceyboy24804

--- Domain/RoadRouteWaypoint.cs
+++ Domain/RoadRouteWaypoint.cs
@@ -1,9 +1,10 @@
+using Colossal.UI.Binding;
 using Unity.Entities;
 using Unity.Mathematics;
 
 namespace RoadSignsTools.Domain
 {
-    public readonly struct RoadRouteWaypoint
+    public readonly struct RoadRouteWaypoint : IJsonWritable
     {
         public RoadRouteWaypoint(Entity segment, float3 position, float curvePosition)
         {
@@ -17,5 +18,17 @@ namespace RoadSignsTools.Domain
         public float3 Position { get; }
 
         public float CurvePosition { get; }
+
+        public void Write(IJsonWriter writer)
+        {
+            writer.TypeBegin(typeof(RoadRouteWaypoint).FullName);
+            writer.PropertyName(nameof(Segment));
+            writer.Write(Segment);
+            writer.PropertyName(nameof(Position));
+            writer.Write(Position);
+            writer.PropertyName(nameof(CurvePosition));
+            writer.Write(CurvePosition);
+            writer.TypeEnd();
+        }
     }
 }
--- Domain/SavedRouteRecord.cs
+++ Domain/SavedRouteRecord.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
+using Colossal.UI.Binding;
 using Unity.Entities;
 
 namespace RoadSignsTools.Domain
 {
-    public sealed class SavedRouteRecord
+    public sealed class SavedRouteRecord : IJsonWritable
     {
         public long RouteId { get; set; }
         public string DisplayTitle { get; set; }
@@ -42,5 +44,92 @@ namespace RoadSignsTools.Domain
         public int SegmentCount => OrderedSegmentIds.Count;
 
         public int WaypointCount => Waypoints.Count;
+
+        public void Write(IJsonWriter writer)
+        {
+            writer.TypeBegin(typeof(SavedRouteRecord).FullName);
+            writer.PropertyName(nameof(RouteId));
+            writer.Write(RouteId);
+            writer.PropertyName(nameof(DisplayTitle));
+            writer.Write(DisplayTitle);
+            writer.PropertyName(nameof(Mode));
+            writer.Write(Enum.GetName(typeof(RoadRouteToolMode), Mode));
+            writer.PropertyName(nameof(BaseInputValue));
+            writer.Write(BaseInputValue);
+            writer.PropertyName(nameof(RouteCode));
+            writer.Write(RouteCode);
+            writer.PropertyName(nameof(RoutePrefixType));
+            writer.Write(RoutePrefixType);
+            writer.PropertyName(nameof(RouteNumberPlacement));
+            writer.Write(Enum.GetName(typeof(RouteNumberPlacement), RouteNumberPlacement));
+            writer.PropertyName(nameof(CreatedAtUtcTicks));
+            writer.Write(CreatedAtUtcTicks);
+            writer.PropertyName(nameof(UpdatedAtUtcTicks));
+            writer.Write(UpdatedAtUtcTicks);
+            writer.PropertyName(nameof(LastAppliedUtcTicks));
+            writer.Write(LastAppliedUtcTicks);
+            writer.PropertyName(nameof(IsDeleted));
+            writer.Write(IsDeleted);
+            writer.PropertyName(nameof(IsUserDefinedTitle));
+            writer.Write(IsUserDefinedTitle);
+            writer.PropertyName(nameof(Notes));
+            writer.Write(Notes);
+            writer.PropertyName(nameof(StartDistrictName));
+            writer.Write(StartDistrictName);
+            writer.PropertyName(nameof(EndDistrictName));
+            writer.Write(EndDistrictName);
+            writer.PropertyName(nameof(StartRoadName));
+            writer.Write(StartRoadName);
+            writer.PropertyName(nameof(EndRoadName));
+            writer.Write(EndRoadName);
+            writer.PropertyName(nameof(DerivedDisplayCorridor));
+            writer.Write(DerivedDisplayCorridor);
+            writer.PropertyName(nameof(DistrictSummary));
+            writer.Write(DistrictSummary);
+            writer.PropertyName(nameof(StartAnchorSegment));
+            writer.Write(StartAnchorSegment);
+            writer.PropertyName(nameof(EndAnchorSegment));
+            writer.Write(EndAnchorSegment);
+            writer.PropertyName(nameof(StartAnchorCurvePosition));
+            writer.Write(StartAnchorCurvePosition);
+            writer.PropertyName(nameof(EndAnchorCurvePosition));
+            writer.Write(EndAnchorCurvePosition);
+            writer.PropertyName(nameof(StartAnchorPositionX));
+            writer.Write(StartAnchorPositionX);
+            writer.PropertyName(nameof(StartAnchorPositionY));
+            writer.Write(StartAnchorPositionY);
+            writer.PropertyName(nameof(StartAnchorPositionZ));
+            writer.Write(StartAnchorPositionZ);
+            writer.PropertyName(nameof(EndAnchorPositionX));
+            writer.Write(EndAnchorPositionX);
+            writer.PropertyName(nameof(EndAnchorPositionY));
+            writer.Write(EndAnchorPositionY);
+            writer.PropertyName(nameof(EndAnchorPositionZ));
+            writer.Write(EndAnchorPositionZ);
+            writer.PropertyName(nameof(LastKnownResolvedSegmentCount));
+            writer.Write(LastKnownResolvedSegmentCount);
+            writer.PropertyName(nameof(Waypoints));
+            writer.ArrayBegin(Waypoints.Count);
+            foreach (var waypoint in Waypoints)
+            {
+                waypoint.Write(writer);
+            }
+            writer.ArrayEnd();
+            writer.PropertyName(nameof(OrderedSegmentIds));
+            writer.ArrayBegin(OrderedSegmentIds.Count);
+            foreach (var segmentId in OrderedSegmentIds)
+            {
+                writer.Write(segmentId);
+            }
+            writer.ArrayEnd();
+            writer.PropertyName(nameof(OriginalStreetNamesSnapshot));
+            writer.ArrayBegin(OriginalStreetNamesSnapshot.Count);
+            foreach (var streetName in OriginalStreetNamesSnapshot)
+            {
+                writer.Write(streetName);
+            }
+            writer.ArrayEnd();
+            writer.TypeEnd();
+        }
     }
 }
